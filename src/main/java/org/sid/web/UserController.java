7b8438112cede15c8e1a2aabc1296fea01a01d27
HILALI Ahmed <ahmedhilali07@gmail.com>
2020-05-26 21:15:43 +0000

    26/05/2020 | 21:15 | Merging master & Features/Task1

--- src/main/java/org/sid/web/UserController.java
+++ src/main/java/org/sid/web/UserController.java
@@ -35,7 +35,6 @@ public class UserController {
 		return "/admin/index";
 	}
 
-	@GetMapping("/user/users")
 	private UtilisateurRepository utilisateurRepository ;
 	//private IUtilisateurMetier utilisateurMetier ;
 	
