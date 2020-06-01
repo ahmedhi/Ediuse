573982f5b0c6476691f18b1da8afbcac60772f83
ASKOUR Hamza <hamza0askour@gmail.com>
2020-06-01 14:13:39 +0200

    1/06/2020 | 12:00 | cut File excel and save data in data base

--- src/main/java/org/sid/metier/IUserMetier.java
+++ src/main/java/org/sid/metier/IUserMetier.java
@@ -3,6 +3,7 @@ package org.sid.metier;
 import java.util.List;
 
 import org.sid.entities.User;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface IUserMetier {
 	User createUser(User user );
@@ -10,5 +11,6 @@ public interface IUserMetier {
 	List<User> getAllUsers();
 	User getUserById(long id);
 	void deleteUser(User user);
+	public boolean saveDataFromUpload(MultipartFile file);
 
 }
--- src/main/java/org/sid/metier/UserMetierImpl.java
+++ src/main/java/org/sid/metier/UserMetierImpl.java
@@ -1,13 +1,30 @@
 package org.sid.metier;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.sid.dao.UserRepository;
+import org.sid.entities.DocCompany;
 import org.sid.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
 @Service
 @Transactional
@@ -38,6 +55,93 @@ public class UserMetierImpl implements IUserMetier {
 	@Override
 	public void deleteUser(User user) {
 		userRepository.delete(user);
+	}
+	
+	@Override
+	public boolean saveDataFromUpload(MultipartFile file) {
+		boolean isFlag = false ; 
+		String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+		if(extension.equalsIgnoreCase("json")) {
+			isFlag = readDataFromJson(file);
+		}else if (extension.equalsIgnoreCase("csv")) {
+			isFlag = readDataFromCsv(file);
+		}else if (extension.equalsIgnoreCase("xls") || extension.equalsIgnoreCase("xlsx") ) {
+			isFlag = readDataFromExcel(file);
+		}
+		return isFlag ;
+	}
+
+	private boolean readDataFromExcel(MultipartFile file) {
+		Workbook workbook = getWorkBook(file);
+		Sheet sheet =  workbook.getSheetAt(0);
+		Iterator<Row> rows = sheet.iterator();
+		rows.next();
+		while(rows.hasNext()) {
+			Row row = rows.next();
+			User user  = new User() ;
+			
+				user.setLoginUser(row.getCell(0).getStringCellValue());
+			
+				user.setLastNameUser(row.getCell(1).getStringCellValue());
+			
+				user.setFirstNameUser(row.getCell(2).getStringCellValue());
+			
+				user.setPwdUser(row.getCell(3).getStringCellValue());
+			
+				user.setRole(row.getCell(4).getStringCellValue());
+			
+			user.setFileType(FilenameUtils.getExtension(file.getOriginalFilename()));
+			userRepository.save(user);
+		}
+		return false;
+	}
+
+	private Workbook getWorkBook(MultipartFile file) {
+		Workbook  workbook = null ;
+		String  extension = FilenameUtils.getExtension(file.getOriginalFilename());
+		try{
+			if(extension.equalsIgnoreCase("xlsx")) {
+				workbook  = new XSSFWorkbook(file.getInputStream());
+			}else if (extension.equalsIgnoreCase("xls")) {
+				workbook  = new HSSFWorkbook(file.getInputStream());
+			}
+		}catch(Exception e ) {
+			e.printStackTrace();
+		}
+		return workbook;
+	}
+
+	private boolean readDataFromCsv(MultipartFile file) {
+		try {
+			InputStreamReader reader = new InputStreamReader(file.getInputStream());
+			CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build();
+			List<String[]> rows = csvReader.readAll();
+			for (String [] row : rows) {
+
+				userRepository.save(new User(row[0], row[1],row[2],row[3],row[4],FilenameUtils.getExtension(file.getOriginalFilename())));
+			}
+			return true ;
+		}
+		catch(Exception e) {
+			return false ;
+		}
+	}
+
+	private boolean readDataFromJson(MultipartFile file) {
+		try {
+			InputStream inputStream = file.getInputStream();
+			ObjectMapper mapper  = new  ObjectMapper() ;
+			List<User> users = Arrays.asList(mapper.readValue(inputStream, User[].class));
+			if(users != null && users.size()>0) {
+				for(User user: users) {
+					user.setFileType(FilenameUtils.getExtension(file.getOriginalFilename()));
+					userRepository.save(user);
+				}
+			}
+			return true ;
+		}catch(Exception e ) {
+			return false;
+		}
 	}
 
 }
